71a66e736e365ba3657aa0f68fc630918ac6a3ca
snirfain <72972397+snirfain@users.noreply.github.com>
2026-04-12 11:33:30 +0300

    Update environment variables and configure Vite server port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const PORT = process.env.PORT || 3000
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(PORT),
+  },
 })
